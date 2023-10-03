8fa390f576ff1fd4a381260a18fce038959b97a9
Gennady Sidorenko <sidorenqo@gmail.com>
2023-10-03 20:33:05 +0500

    Тернарный оператор

--- app.js
+++ app.js
@@ -1,57 +1,30 @@
-// SWITCH STATEMENT - позволяет избежать дублирование if
+// Тернарный оператор
 
-const role = "admin";
+const bmwX3Price = 1000000;
+// const fordFocusPrice = 10000;
 
-if (role === "manager") {
-  console.log("Это менеджер!");
-} else if (role === "admin") {
-  console.log("Это администратор!");
-} else if (role === "SEO") {
-  console.log("Это СЕО компании!");
+const budget = 20000;
+
+let message;
+
+if (budget > bmwX3Price) {
+  message = "BMW";
 } else {
-  console.log("Мы не знаем кто ты");
+  message = "НЕХВАТАЕТ ДЕНЕГ!";
 }
 
-// Теперь запишем выражения выше с помощью SWITCH
+// // Для упрощения вводим тернарный оператор ?
+// const str =  10 > 0 ? 'Больше нуля' : 'Меньше нуля'
 
-switch (role) {
-  case "manager": // 'role' === строго равно 'manager'
-    console.log("Это менеджер!");
-    break;
-  case "admin":
-    console.log("Это админ!");
-    break;
-  case "SEO":
-    console.log("Это СЕО компании!");
-    break;
-  default:
-    console.log("Мы не знаем кто ты");
-}
+// // Запись выше полностью равно этой:
 
-// Объеденение в SWITCH
+// if (10 > 0) {
+//   message = "Больше нуля";
+// } else {
+//   message = "Меньше нуля";
+// }
+// console.log(`Я хочу купить ${message} `);
 
-switch (role) {
-  case "manager": // 'role' === строго равно 'manager'
-  case "admin":
-    console.log("Не руководитель!");
-    break;
-  case "SEO":
-    console.log("Руководитель!");
-    break;
-  default:
-    console.log("Мы не знаем кто ты");
-}
-
-// ЕЩЕ ПРИМЕР
-const num = 1;
-
-switch (true) {
-  case num > 0: // true === num > 0
-    console.log("Положительный");
-    break;
-  case num < 0:
-    console.log("Отрицательный");
-    break;
-  default:
-    console.log("нолЬ!");
-}
+console.log(
+  `Я хочу купить ${budget > bmwX3Price} ? 'BMW' : "НЕХВАТАЕТ ДЕНЕГ!"`
+);
