dd6c9fa106bbd113fd802ad35eee7b40c15cef68
Gennady Sidorenko <sidorenqo@gmail.com>
2023-10-03 20:07:51 +0500

    Разбираем SWITCH

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
-const secretNumber = "7";
+// SWITCH STATEMENT - позволяет избежать дублирование if
 
-if (Number(secretNumber) === 7) {
-  // Строгое сравнение
-  console.log("угадал строго"); // Нужно привести результат к числу ЯВНО, иначе в консоль ничего не выведется
+const role = "admin";
+
+if (role === "manager") {
+  console.log("Это менеджер!");
+} else if (role === "admin") {
+  console.log("Это администратор!");
+} else if (role === "SEO") {
+  console.log("Это СЕО компании!");
+} else {
+  console.log("Мы не знаем кто ты");
 }
 
-if (secretNumber == 7) {
-  // Нестрогое сравнение. Хорошее правило НЕ ИСПОЛЬЗОВАТЬ!
-  console.log("угадал не строго");
+// Теперь запишем выражения выше с помощью SWITCH
+
+switch (role) {
+  case "manager": // 'role' === строго равно 'manager'
+    console.log("Это менеджер!");
+    break;
+  case "admin":
+    console.log("Это админ!");
+    break;
+  case "SEO":
+    console.log("Это СЕО компании!");
+    break;
+  default:
+    console.log("Мы не знаем кто ты");
 }
 
-const q = Number(prompt("Введите число"));
-if (q === 7) {
-  console.log("!");
+// Объеденение в SWITCH
+
+switch (role) {
+  case "manager": // 'role' === строго равно 'manager'
+  case "admin":
+    console.log("Не руководитель!");
+    break;
+  case "SEO":
+    console.log("Руководитель!");
+    break;
+  default:
+    console.log("Мы не знаем кто ты");
+}
+
+// ЕЩЕ ПРИМЕР
+const num = 1;
+
+switch (true) {
+  case num > 0: // true === num > 0
+    console.log("Положительный");
+    break;
+  case num < 0:
+    console.log("Отрицательный");
+    break;
+  default:
+    console.log("нолЬ!");
 }
