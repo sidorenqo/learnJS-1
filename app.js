267d52e2322400326ae635de8c42d4b516558c03
Gennady Sidorenko <sidorenqo@gmail.com>
2023-10-03 19:38:34 +0500

    Проверка на равенство

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
-/**
- * Вася положил 12000$ на вклад 7% годовых с капитализацией 1 раз в месяц.
- * Вывесли в консоль, сможет ли он купить дом за 13500$ через 2 года после снятия вклада. И остаток после покупки.
- *
- *
- * формалула сложного процента:
- * Итог = Сумма * (1 + Ставка в месяц не в %) ^ срок в месяцах
- */
+const secretNumber = "7";
 
-const totalMoney = 12000;
-const percentagePerMonth = 0.07; // Приводим к числу
-// console.log(percentagePerMonth);
+if (Number(secretNumber) === 7) {
+  // Строгое сравнение
+  console.log("угадал строго"); // Нужно привести результат к числу ЯВНО, иначе в консоль ничего не выведется
+}
 
-const moneyWithInsert = totalMoney * (1 + percentagePerMonth / 12) ** 24;
-// console.log(moneyWithInsert)
-const priceNewHouse = 13500;
+if (secretNumber == 7) {
+  // Нестрогое сравнение. Хорошее правило НЕ ИСПОЛЬЗОВАТЬ!
+  console.log("угадал не строго");
+}
 
-if (moneyWithInsert >= priceNewHouse) {
-  console.log(
-    `Вася сможет купить себе новый дом, так как заработает ${moneyWithInsert} $`
-  );
-} else {
-  console.log(
-    `Вася не купит новый дом. Он заработает всего ${moneyWithInsert}`
-  );
+const q = Number(prompt("Введите число"));
+if (q === 7) {
+  console.log("!");
 }
