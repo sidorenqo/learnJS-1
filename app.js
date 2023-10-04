58b13d6b5624e796fced3944b9a96454e72ec6d1
Gennady Sidorenko <sidorenqo@gmail.com>
2023-10-04 20:25:09 +0500

    Функции внутри функций, переиспользование функций

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-// УСЛОВИЕ В ФУНКЦИИ
+// ФУНКЦИИ ВНУТРИ ФУНКЦИЙ
 
-function canAccessWebsite(age) {
-  if (age < 18) {
-    return false;
-  } // При использовании в функции, мы опускаем else. Т.к. после return функция прекращает работу
-  return true;
+// На примере задачи: Мы служба доставки придумали стартап в котором люди могут обмениваться подарками. Цена за доставку будет меняться в зависимости от веса и дальности доставки
+
+const KG_IN_USD = 7; // Стоимость одного кг веса при доставке
+const KM_IN_USD = 5; // Стоимость одного километра пути при доставке
+
+function calculateW(present) {
+  // расчитывает стоимость по весу, параметр вес
+  return present * KG_IN_USD; // возвращаем вес умноженный на 7 долларов
 }
 
-console.log(canAccessWebsite(18));
+function calculateKm(distance) {
+  // расчитывает стоимость по весу, параметр вес
+  return distance * KM_IN_USD; // возвращаем вес умноженный на 7 долларов
+}
 
-// Перепишем эту функцию в стрелочную
+function getExchangePrice(present1, present2, distance) {
+  // Функция считает стоимость доставки, параметры -  вес подарок 1, вес подарок 2, расстояние
+  /** ТУТ Запись, если бы мы не использовали дополнительные функции!
+ *const price1 = present1 * KG_IN_USD;
+  const price2 = present2 * KG_IN_USD;
+  const distancePrice = distance * KM_IN_USD;
+  return price1 + price2 + distancePrice;
+ */
+  const price1 = calculateW(present1);
+  const price2 = calculateW(present2);
+  const distancePrice = calculateKm(distance);
+  return price1 + price2 + distancePrice;
+}
 
-const canAccessWebsite2 = (age) => (age < 18 ? false : true); // Тернарный оператор и true/false можно не писать
-console.log(canAccessWebsite2(22));
+console.log(getExchangePrice(2, 8, 25));
