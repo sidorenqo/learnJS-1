5b4b9727cb70c23e34c1fbd529e3156062e0209a
Gennady Sidorenko <sidorenqo@gmail.com>
2023-10-04 15:19:59 +0500

    Булева логика

--- app.js
+++ app.js
@@ -1,45 +1,30 @@
-/**
- * Методом prompt получите ответ пользователя на вопрос "Сколько будет 7 + или - 15?". Если ответ верен, введите в консоле "Успех", если нет - "Вы робот!",
- * а если он введет "Я не робот", то тоже успех.
- *
- */
+const isAdmin = true;
+const canWrite = true;
 
-let responseUser = prompt("Сколько будет 7 + или - 15?");
+console.log(`Системный файл ${isAdmin && canWrite}`);
+console.log(`Обчный файл ${isAdmin || canWrite}`);
+console.log(`Инвертируем права админа ${!isAdmin}`); // НЕ
 
-// Решение 1
-if (responseUser == -8 || responseUser == 23) {
-  console.log("Успех!");
-} else if (responseUser == "Я не робот") {
-  console.log("Успех!");
-} else {
-  console.log("Вы ррррробот!");
-}
+const isEdited = true;
+const isSuperAdmin = true;
+console.log(
+  `Файл с редактированием ${isAdmin && canWrite && (!isEdited || isSuperAdmin)}`
+);
 
-// Решение 2
-switch (true) {
-  case responseUser === -8:
-  case Number(responseUser) === 22:
-  case Number(responseUser) === "Я не робот":
-    console.log("Успех!");
-    break;
-  default:
-    console.log("Вы ррррробот!");
-}
+// Операторы с другими типами
 
-// Решение 3
+console.log("Вася || Олег"); // Или выводит первое правдивое или последнее ложное
+console.log("false || Олег"); // Или выводит первое правдивое или последнее ложное
+console.log("Вася || false"); // Или выводит первое правдивое или последнее ложное
 
-if (responseUser === "Я не робот!") {
-  console.log("Успех!");
-} else {
-  const responseUserNum = Number(responseUser); // Чтобы дважды не повторять код как в решении 2, вводим вторую переменную и конвертируем в число
-  switch (
-    responseUserNum // Можно запустить switch внутри if else!!
-  ) {
-    case 22:
-    case -8:
-      console.log("Успех!");
-      break;
-    default:
-      console.log("Вы робот!");
-  }
-}
+console.log("Вася" && "Олег"); // И вывоит последнее прадивое или первое ложное
+console.log(false && "Олег"); // И вывоит последнее прадивое или первое ложное
+console.log("Вася" && false); // И вывоит последнее прадивое или первое ложное
+
+let a;
+const userName = a || "Петя";
+console.log(userName);
+
+const itsAdmin = false;
+const fileName = itsAdmin && "file.mp4";
+console.log(fileName);
