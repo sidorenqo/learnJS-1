72fe7aa98447a811122769bd3c7458f68d14363f
Gennady Sidorenko <sidorenqo@gmail.com>
2023-10-04 19:52:42 +0500

    Как записывать условие в функциях?

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-function toPower(num, power) {
-  const res = num ** power;
-  return res;
+// УСЛОВИЕ В ФУНКЦИИ
+
+function canAccessWebsite(age) {
+  if (age < 18) {
+    return false;
+  } // При использовании в функции, мы опускаем else. Т.к. после return функция прекращает работу
+  return true;
 }
 
-console.log(toPower(2, 3));
+console.log(canAccessWebsite(18));
+
+// Перепишем эту функцию в стрелочную
+
+const canAccessWebsite2 = (age) => (age < 18 ? false : true); // Тернарный оператор и true/false можно не писать
+console.log(canAccessWebsite2(22));
