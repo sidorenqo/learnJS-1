858e04996def20ba3aa48c3500226a7a17122b35
Gennady Sidorenko <sidorenqo@gmail.com>
2023-10-05 20:56:15 +0500

    Знакомство с массивами

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
-/*
-	Пользователь:
-	- Возраст
-	- Наличие работы
-	- Деньги
-	Нужно проверить может ли он купить новый MacBook за 2000$?
-	Он может брать не только свои деньги, но и взять кредит.
-	Ему дадут 500$, только если ему больше 24-х лет и он
-	имеет работу, 100$ если ему просто больше 24-х лет и 0 в
-	ином случае.
-	Напишите функцию, которая принимает данные пользователя
-	и товара и возвращает true или false;
-*/
+const role1 = "admin";
+const role2 = "user";
+const role3 = "superUser";
 
-function computeCredit(age, hasJob = false) {
-  switch (true) {
-    case age > 24 && hasJob:
-      return 500;
-    case age > 24:
-      return 100;
-    default:
-      return 0;
-  }
-}
-console.log(`Клиенту одобрен кредит в размере: ${computeCredit(29, true)} $`);
+const roles = ["admin", "user", "superUser"];
+const userInfo = ["Аня", 25];
 
-function canBuy(productPrice, age, money, hasJob = false) {
-  const creditMoney = computeCredit(age, hasJob);
-  return productPrice <= money + creditMoney;
-}
+console.log(roles);
 
-console.log(`Можем продать макбук клиенту : ${canBuy(2000, 25, 1000)}`);
+console.log(roles[3]);
+
+console.log(roles.length);
+console.log(roles.length - 1); // Получение последнего элемента
+
+console.log(roles.at(0)); // Современный синтаксис получения последнего элемента
+console.log(roles.at(0)); // Например получение последнего элемента выглядит так
+
+// Что может быть элементом массива
+const usersAge = [2040 - 2023, 20 - "6", 10 > 0 ? 5 : 0];
+console.log(usersAge);
+
+// Альтернативный способ создания массива
+
+const usersNames = new Array("Вася", "Петя", "Катя");
+console.log(usersNames);
