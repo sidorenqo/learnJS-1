c9f544db068df52b6d2ed089b0a3a85b622c35c7
Gennady Sidorenko <sidorenqo@gmail.com>
2023-10-11 10:08:58 +0500

    Модификация строк в массив и обратно

--- app.js
+++ app.js
@@ -1,38 +1,8 @@
-// SLICE SPLISE CONCAT REVERS
-
+// Модификация строк в массив и обратно.
 let roles = ["user", "admin", "manager", "superuser"];
 
-// SLICE Не модифицирует исходный массив. В качестве первого аргумента принимает начальный индекс, в качестве второго (если он есть) - конечный.
-const res = roles.slice(2); // Взят второй элемент, массив отрезан до конца, присвоен результату выполнения
+const url = "auth/user/admin";
+const res = url.split("/");
 console.log(res);
 
-const res2 = roles.slice(2, 3); // В качестве начального элемента взят 2 второй элемент, в качестве последнего третьего элемента, НО НЕ ВКЛЮЧАЯ ЕГО
-console.log(res2);
-
-const res3 = roles.slice(-1); // БЕРЕТ ПОСЛЕДНИЙ ЭЛЕМЕНТ МАССИВА в зависимости от числа которое указано в методе ( например если указано -2, то возьмет последние 2 елемента)
-console.log(res3);
-
-const res4 = roles.slice(1, -2); // Первая цифра - считаем сначала, вторая сконца
-console.log(res4);
-
-// SPLICE - Модифицирует исходный массив. В качестве первого аргумента принимает начальный индекс, в качестве второго (если он есть) - длину массива.
-roles = ["user", "admin", "manager", "superuser"];
-
-const res5 = roles.splice(2); // Изменяет длину начального массива, модифицирует массив
-console.log(res5);
-
-const res6 = roles.splice(2, 2); // Со второго элемента берем 2 элемента
-console.log(res6);
-
-// REVERSE - переварачивает массив. Модифицирует исходный массив
-
-roles = ["user", "admin", "manager", "superuser"];
-
-const res8 = roles.reverse();
-console.log(res8);
-
-// CONCAT - соединение 2х массивов
-roles = ["user", "admin", "manager", "superuser"];
-const newRoles = ["sysadmin", "developer"];
-const res9 = roles.concat(newRoles);
-console.log(res9);
+console.log(roles.join("-"));
