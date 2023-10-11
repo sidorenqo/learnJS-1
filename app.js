aed38a96b5052cc445e674a50458d04168b34925
Gennady Sidorenko <sidorenqo@gmail.com>
2023-10-11 16:12:09 +0500

    Деструктуризация массивов

--- app.js
+++ app.js
@@ -1,49 +1,15 @@
-// Модификация строк в массив и обратно.
-// let roles = ["user", "admin", "manager", "superuser"];
+// Деструктуризация массивов
 
-// const url = "auth/user/admin";
-// const res = url.split("/");
-// console.log(res);
+const userData = ["Антон", 18, "Москва"];
 
-// console.log(roles.join("-"));
-
-/**
- *  Дан список задач
- * const tasks = ['Задача 1']
- * Сделать функции: Добавление задачи в конец массива
- * Удаление задачи по названию
- * Перенос задачи в начало списка по названию
- * ВСЕГДА МЕНЯЕМ ИСХОДНЫЙ МАССИВ!
- */
-
-const tasks = ["Задача 1"];
-
-function addTasks(el) {
-  tasks.push(el);
+function getData() {
+  return ["Антон", 18, "Москва"];
 }
+// Запись без деструктуризации
+// const userName = getData()[0];
+// const age = getData()[1];
+// const city = getData()[2];
 
-function delTasks(el) {
-  const indexElement = tasks.indexOf(el);
-  if (indexElement === -1) {
-    return;
-  }
-  return tasks.splice(indexElement, 1);
-}
+const [userName, age, city] = getData();
 
-function transferTasks(el) {
-  const result = delTasks(el);
-  if (!result) {
-    return;
-  }
-  tasks.unshift(result[0]);
-}
-
-addTasks("Задача 2");
-addTasks("Задача 3");
-console.log(tasks);
-
-delTasks("Задача 2");
-console.log(tasks);
-
-transferTasks("Задача 3");
-console.log(tasks);
+console.log(userName, age, city);
