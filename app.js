6e94acc9d665c364d5e864e3f2cf07a6f24ec6c8
Gennady Sidorenko <sidorenqo@gmail.com>
2023-10-18 19:53:28 +0500

    [Задача] Делаем из обычной функции стрелочную

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-// https
-// purpleschool.ru
-// /course/javascript
+// function power(pow) {
+//   return function (num) {
+//     return num ** pow;
+//   };
+// }
 
-const url = "https://purpleschool.ru/course/javascript";
+// const powerOfTwo = power(2);
+// console.log(powerOfTwo(5));
 
-function getUrlParts(url) {
-  const [protocol, _, domen, ...way] = url.split("/");
-  if (protocol === "https:" || protocol === "http:") {
-    // Дополнительная проверка передаваемой ссылки
-    if (!host.includes(".")) {
-      return;
-    }
-    console.log(protocol, _, domen, way);
-    console.log(`Протокол: ${protocol.split(":")[0]}`);
-    console.log(`Адрес сайта: ${domen}`);
-    console.log(`Путь внутри сайта: /${way.join("/")}`);
-  }
-}
+// // Мы исполнили функцию с аргументом 2. И в powerOfTwo положили результат. Результатом является еще одна функция.
 
-getUrlParts(url);
+// // Еслии в будующем нам понадобится возводить число уже в треью степень, мы легко может переделать выражение:
+
+// const powerOfTree = power(3);
+// console.log(powerOfTree(5));
+
+// // Можем сделать более короткую запись: (например мы хотим возвеси 4 в 5 степень)
+
+// console.log(power(5)(4));
+
+const power = (pow) => (num) => num ** pow;
+const powerOfTwo = power(2);
+console.log(powerOfTwo(5));
+
+const powerOfThree = power(3);
+console.log(powerOfThree(5));
+
+console.log(power(5)(4));
